c13ca1bf087b933ff74245f024bdc28e50d127be
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-12 14:34:51 +0300

    Time: 15 ms (77.78%) | Memory: 19.7 MB (23.45%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,7 +2,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         c = Counter(magazine)
         for char in ransomNote:
-            print(c[char])
+          
             if char in c:
                 if c[char] > 1:
                   c[char] = c[char] - 1
