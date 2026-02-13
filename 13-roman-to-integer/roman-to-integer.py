06bf4630a0de452cfcf166762bf41d5963f65465
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-13 14:11:03 +0300

    Refactor romanToInt method by cleaning comments
    Removed docstring and comments for clarity.

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Mapping Roman numerals to their values
+       
         roman_to_value = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000
@@ -14,7 +10,7 @@ class Solution(object):
         n = len(s)
         
         for i in range(n):
-            # Check if this character needs to be subtracted
+           
             if i < n - 1 and roman_to_value[s[i]] < roman_to_value[s[i + 1]]:
                 total -= roman_to_value[s[i]]
             else:
