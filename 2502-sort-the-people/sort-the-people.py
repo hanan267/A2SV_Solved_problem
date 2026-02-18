165a43f1db6e48c734b79d76f53f7f15694d844f
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-18 17:24:12 +0300

    Time: 747 ms (5.12%) | Memory: 19.5 MB (73.50%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -3,18 +3,21 @@ class Solution:
         # height_map = {}
         # for i in range(len(heights)):
         #     height_map[heights[i]] = names[i]
-        # for i in range(len(names)-1):
-        #     if heights[i] < heights[i+1]:
-        #         heights[i], heights[i+1] =heights[i+1], heights[i]
-        #         names[i], names[i+1] = names[i+1], names[i]
-        # return (names)
-        n = len(heights)
-        for i in range(n):
-            for j in range(i+1):
-                if heights[j] < heights[i]:
-                    heights[j], heights[i] = heights[i], heights[j]
-                    names[j], names[i] = names[i], names[j]
-        return names
+        found = False
+        n = len(names)
+        for j in range(n):
+            for i in range(n-j-1):
+                if heights[i] < heights[i+1]:
+                    heights[i], heights[i+1] = heights[i+1], heights[i]
+                    names[i], names[i+1] = names[i+1], names[i]
+        return (names)
+        # n = len(heights)
+        # for i in range(n):
+        #     for j in range(i+1):
+        #         if heights[j] < heights[i]:
+        #             heights[j], heights[i] = heights[i], heights[j]
+        #             names[j], names[i] = names[i], names[j]
+        # return names
 
 
 
