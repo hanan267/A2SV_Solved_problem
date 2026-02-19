52079dcf17884c4f77e73454581a604c1ba1cb75
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-19 17:56:19 +0300

    Time: 674 ms (5.15%) | Memory: 19.8 MB (17.51%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -3,14 +3,15 @@ class Solution:
         # height_map = {}
         # for i in range(len(heights)):
         #     height_map[heights[i]] = names[i]
-        found = False
-        n = len(names)
-        for j in range(n):
-            for i in range(n-j-1):
-                if heights[i] < heights[i+1]:
-                    heights[i], heights[i+1] = heights[i+1], heights[i]
-                    names[i], names[i+1] = names[i+1], names[i]
-        return (names)
+            # buble sort
+        # n = len(names)
+        # for j in range(n):
+        #     for i in range(n-j-1):
+        #         if heights[i] < heights[i+1]:
+        #             heights[i], heights[i+1] = heights[i+1], heights[i]
+        #             names[i], names[i+1] = names[i+1], names[i]
+        # return (names)
+            # insertion sort
         # n = len(heights)
         # for i in range(n):
         #     for j in range(i+1):
@@ -18,6 +19,35 @@ class Solution:
         #             heights[j], heights[i] = heights[i], heights[j]
         #             names[j], names[i] = names[i], names[j]
         # return names
+        
+        #   insertion sort
+        
+        n = len(names)
+        for i in range(n):
+            for j  in range(i, 0, -1):
+                if heights[j] > heights[j-1]:
+                    heights[j], heights[j-1] = heights[j-1], heights[j]
+                    names[j], names[j-1] = names[j-1], names[j]
+
+                
+        return names
+
+                # counting sort
+        # n = len(heights)
+        # big = max(heights)
+        # count = [0]*big
+        # # print(big)
+        # for i in range(n):
+        #     count[heights[i]-1] += 1
+        # res = []
+        # for i in range(len(count)):
+        #     if count[i] != 0:
+        #         for j in range(count[i]):
+        #             res.append(i+1)
+                
+        # return res
+        
+
 
 
 
