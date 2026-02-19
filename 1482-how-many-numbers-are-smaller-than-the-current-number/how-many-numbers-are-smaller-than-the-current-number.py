694d50b2e8f4c46514202d153ced5b9fd7a9b09c
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-19 13:46:59 +0300

    Time: 3 ms (76.91%) | Memory: 19.4 MB (46.19%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        # optimized 
-            Sorted = sorted(nums)  # O(nlogn) , O(n)
-            map = {}  # O(1), O(n)
-            for i in range(len(nums)): # O(n), O(1)
-                if Sorted[i] not in map:
-                    map[Sorted[i]] = i
-            for i in range(len(nums)): # O(n), O(1)
-                nums[i] = map[nums[i]]
-            return nums
+        # # optimized 
+        #     Sorted = sorted(nums)  # O(nlogn) , O(n)
+        #     map = {}  # O(1), O(n)
+        #     for i in range(len(nums)): # O(n), O(1)
+        #         if Sorted[i] not in map:
+        #             map[Sorted[i]] = i
+        #     for i in range(len(nums)): # O(n), O(1)
+        #         nums[i] = map[nums[i]]
+        #     return nums
 
-            # time  O(nlogn+n+n) = O(nlogn)
-            #  space = O(2n) = O(n)
+        #     # time  O(nlogn+n+n) = O(nlogn)
+        #     #  space = O(2n) = O(n)
 
         # brute force approach
              # iterate from 0 - n/ lenth
@@ -46,6 +46,35 @@ class Solution:
         # time complexity: O(n^2)
         # space complexity: O(n)
         
-    
+        Sorted = sorted(nums)
 
-             
+        n = len(nums)
+        res = []
+        small_nums = {}
+        for i in range(n):
+           if Sorted[i] not in small_nums:
+            small_nums[Sorted[i]] = i
+        # print(small_nums)
+        return [small_nums[nums[i]] for i in range(n)]
+        
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
