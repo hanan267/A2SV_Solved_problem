ff6de31d2f6e98062ffe80310a9234f59a240ff4
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-23 17:27:41 +0300

    Time: 3 ms (82.75%) | Memory: 20.4 MB (73.48%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,7 +6,10 @@ class Solution:
         pointer = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[pointer], nums[i] = nums[i], nums[pointer]
+                temp = nums[pointer]
+                nums[pointer] = nums[i]
+                nums[i] = temp
+                # nums[pointer], nums[i] = nums[i], nums[pointer]
                 pointer += 1 
         return nums
         
