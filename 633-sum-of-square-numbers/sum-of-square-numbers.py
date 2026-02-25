d1c92bce747683c866d4a7a3a35d1c503089ced9
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-02-25 13:18:21 +0300

    Refactor judgeSquareSum method
    Refactored the judgeSquareSum method to remove commented-out code and improve readability.

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,16 +1,6 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # left = 0
-        # right = int(sqrt(c))
-        # while (left <= right):
-        #    sumSquare = left*left + right*right
-        #    if sumSquare == c:
-        #       return True
-        #    elif sumSquare > c:
-        #        right -= 1
-        #    else:
-        #       left += 1
-        # return False
+
 
         left, right = 0, int(sqrt(c))
         while left<=right:
