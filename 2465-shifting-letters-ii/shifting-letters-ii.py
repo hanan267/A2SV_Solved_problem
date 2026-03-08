f6e8b289dc3c25f7d62eaf051652ef5a1131d3b7
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-03-08 13:21:52 +0300

    Time: 57 ms (65.15%) | Memory: 42.8 MB (76.46%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -2,19 +2,86 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
 
         pref = [0]*(len(s)+1)
-        for left, right, d in shifts:
-            pref[left] += -1 if d else 1
-            pref[right+1] += 1 if d else -1
+        for start, end, dxn in shifts[0:]:
+            pref[start] += 1 if dxn == 1 else -1
+            pref[end+1] += -1 if dxn == 1 else 1
         
-        diff = 0
-        res = [ord(c) - ord("a") for c in s]
-        for i in reversed(range(len(pref))):
-            diff += pref[i]
+        total = 0
+        for i in range(len(pref)):
+            total += pref[i]
+            pref[i] = total
+        
+        red = []
+        for i in range(len(s)):
+            newchar = chr((ord(s[i]) - ord('a') + pref[i]) % 26 + ord('a'))
+            red.append(newchar)
 
-            res[i-1] = (diff + res[i-1]) % 26
+        return "".join(red)
         
-        s = [chr(ord("a") + n) for n in res]
-        return "".join(s)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # pref = [0]*(len(s)+1)
+        # for left, right, d in shifts:
+        #     pref[left] += -1 if d else 1
+        #     pref[right+1] += 1 if d else -1
+        
+        # diff = 0
+        # res = [ord(c) - ord("a") for c in s]
+        # for i in reversed(range(len(pref))):
+        #     diff += pref[i]
+
+        #     res[i-1] = (diff + res[i-1]) % 26
+        
+        # s = [chr(ord("a") + n) for n in res]
+        # return "".join(s)
 
         
         
