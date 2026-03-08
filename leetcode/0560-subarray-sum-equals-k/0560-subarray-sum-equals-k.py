ba482fc6b7011fce2c8fa4cb95c7b37e4361bd30
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-03-08 12:43:22 +0300

    Add solution for Subarray Sum Equals K

--- leetcode/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ leetcode/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,60 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-       
-        prefSum = {0:1}
+
+        remainder = {0:1}
+
         total = 0
         res = 0
         for i in range(len(nums)):
-                total += nums[i]
-                diff = total-k
+            total += nums[i]
+            diff = total-k
 
-                res += prefSum.get(diff, 0)
-                prefSum[total] = prefSum.get(total, 0) + 1
+            if diff in remainder:
+                res += remainder[diff]
+            remainder[total] = remainder.get(total, 0) + 1
         return res
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+        # prefSum = {0:1}
+        # total = 0
+        # res = 0
+        # for i in range(len(nums)):
+        #         total += nums[i]
+        #         diff = total-k
+
+        #         res += prefSum.get(diff, 0)
+        #         prefSum[total] = prefSum.get(total, 0) + 1
+        # return res
