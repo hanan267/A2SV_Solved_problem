de8e7e909600af00b3adbc1b89b4db0472d85d54
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-03-08 12:00:27 +0300

    Add solution for Continuous Subarray Sum

--- leetcode/0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ leetcode/0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,35 +1,35 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
 
-        # remainder = { 0:1 }
+        remainder = { 0:-1 }
 
         
-        # pref = 0
-        # for i in range(1,len(nums)):
-        #     pref += nums[i]
-        #     r = pref%k
-        #     if r not in remainder:
-        #         remainder[r] = i
-        #     elif i - remainder[r] > 1:
-        #         return True
-    
-            
-        # return False
-
-
-
-
-        remainder = {0:-1}
-        total = 0
-
-        for i, n in enumerate(nums):
-            total += n
-            r = total % k
+        pref = 0
+        for i in range(len(nums)):
+            pref += nums[i]
+            r = pref%k
             if r not in remainder:
                 remainder[r] = i
-            elif i-remainder[r] > 1:
+            elif i - remainder[r] > 1:
                 return True
+    
+            
         return False
+
+
+
+
+        # remainder = {0:-1}
+        # total = 0
+
+        # for i, n in enumerate(nums):
+        #     total += n
+        #     r = total % k
+        #     if r not in remainder:
+        #         remainder[r] = i
+        #     elif i-remainder[r] > 1:
+        #         return True
+        # return False
 
 
         
