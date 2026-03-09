7d5e34c1a31a8ab0ca842e9c9118800b7f8f7d6d
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-03-09 16:47:53 +0300

    Time: 3 ms (34.60%) | Memory: 19.3 MB (63.86%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -7,16 +7,26 @@ class Solution:
             ')':'('
         }
 
-        for chars in s:
-            if chars in brackets.values():
-                stack.append(chars)
-            elif chars in brackets.keys():
-                if stack and stack[-1] == brackets[chars]:
+        # for chars in s:
+        #     if chars in brackets.values():
+        #         stack.append(chars)
+        #     elif chars in brackets.keys():
+        #         if stack and stack[-1] == brackets[chars]:
+        #             stack.pop()
+        #         else:
+        #             return False 
+        # return len(stack) == 0
+
+
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            elif char in brackets.keys(): 
+                if stack and stack[-1] == brackets[char]:
                     stack.pop()
                 else:
-                    return False 
+                    return False
         return len(stack) == 0
-
 
        
 
