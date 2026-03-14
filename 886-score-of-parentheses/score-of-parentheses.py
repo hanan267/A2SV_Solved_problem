254e78b905d4ffd44c6cd43255c11ff6d8db87ab
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-03-14 23:13:39 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (44.17%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -1,24 +1,80 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
 
+
         stack = []
         score = 0
-        for par in s:
-            if par == "(":
-                stack.append([par, 0])
-                
-            
-            elif par == ")":
-                
-                _ , vl = stack.pop()
-                # print(vl)
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append([s[i], 0])
+            else:
+                _, val = stack.pop()
                 if stack:
-                    stack[-1][1] += max(2 * vl,1)
+                    stack[-1][1] += max(2*val, 1)
                 else:
-                    score += max(2 * vl,1)
-                
-                
+                    score += max(2*val, 1)
         return score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # score = 0
+        # for par in s:
+        #     if par == "(":
+        #         stack.append([par, 0])
+        #     elif par == ")":
+        #         _ , vl = stack.pop()
+                
+        #         if stack:
+        #             stack[-1][1] += max(2 * vl,1)
+        #         else:
+        #             score += max(2 * vl,1)
+                
+                
+        # return score
             
 
 
