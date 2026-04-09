b4b835b8447523b7f967ea3e56f26475ee478a3c
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-04-09 17:48:30 +0300

    Add solution for Sort the People

--- leetcode/2418-sort-the-people/2418-sort-the-people.py
+++ leetcode/2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if heights[i] < heights[j]:
+                    heights[i], heights[j] = heights[j], heights[i]
+                    names[i], names[j] = names[j], names[i]
+        return names
+
+         # selection sort
+        
         for i in range(n):
             for j in range(i+1, n):
                
