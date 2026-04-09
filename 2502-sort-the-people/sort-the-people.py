77f0b0a8eb5afe23d46ee77fc03aa9cea3d6bf02
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-04-09 17:41:32 +0300

    Time: 416 ms (17.42%) | Memory: 19.7 MB (65.82%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,51 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # height_map = {}
-        # for i in range(len(heights)):
-        #     height_map[heights[i]] = names[i]
-            # buble sort
-        # n = len(names)
-        # for j in range(n):
-        #     for i in range(n-j-1):
-        #         if heights[i] < heights[i+1]:
-        #             heights[i], heights[i+1] = heights[i+1], heights[i]
-        #             names[i], names[i+1] = names[i+1], names[i]
-        # return (names)
-            # insertion sort
-        # n = len(heights)
-        # for i in range(n):
-        #     for j in range(i+1):
-        #         if heights[j] < heights[i]:
-        #             heights[j], heights[i] = heights[i], heights[j]
-        #             names[j], names[i] = names[i], names[j]
-        # return names
-        
-        #   insertion sort
-        
         n = len(names)
         for i in range(n):
-            for j  in range(i, 0, -1):
-                if heights[j] > heights[j-1]:
-                    heights[j], heights[j-1] = heights[j-1], heights[j]
-                    names[j], names[j-1] = names[j-1], names[j]
+            for j in range(i+1, n):
+               
+                if heights[i] < heights[j]:
+                    
+                    names[i], names[j] = names[j], names[i]
+                    heights[i], heights[j] = heights[j], heights[i]
 
-                
         return names
-
-                # counting sort
-        # n = len(heights)
-        # big = max(heights)
-        # count = [0]*big
-        # # print(big)
-        # for i in range(n):
-        #     count[heights[i]-1] += 1
-        # res = []
-        # for i in range(len(count)):
-        #     if count[i] != 0:
-        #         for j in range(count[i]):
-        #             res.append(i+1)
-                
-        # return res
         
 
 
