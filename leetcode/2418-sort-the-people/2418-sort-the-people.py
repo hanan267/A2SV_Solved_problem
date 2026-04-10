4d7f3a6bc8c5236b616419f5920c26d5c35b16ab
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-04-10 14:38:29 +0300

    Add solution for Sort the People

--- leetcode/2418-sort-the-people/2418-sort-the-people.py
+++ leetcode/2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,23 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names)
+        # insertion sort 
+
+        for i in range(1,len(names)):
+            key1 = names[i]
+            key = heights[i]
+            j = i - 1
+
+            while j >= 0 and heights[j] < key:
+                heights[j+1] = heights[j]
+                names[j+1] = names[j]
+                j -= 1
+            heights[j+1] = key
+            names[j+1] = key1
+        return names
+
+
+        # bubble sort
         for i in range(n-1):
             for j in range(i+1, n):
                 if heights[i] < heights[j]:
