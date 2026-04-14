8afce377c84b067149e0fb9487226fc9c30c76fe
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-04-14 10:27:49 +0300

    Add solution for H-Index II

--- leetcode/0275-h-index-ii/0275-h-index-ii.py
+++ leetcode/0275-h-index-ii/0275-h-index-ii.py
@@ -1,6 +1,24 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+
+
+        n = len(citations)
+        if n == 0:
+            return 0
         
+        firstTrueIdx = -1
+        left, right = 0, n-1
+
+        while left <= right:
+
+            mid = (left+right) // 2
+            if citations[mid] >= n - mid:
+                firstTrueIdx = mid 
+                right = mid - 1
+            else:
+                left = mid + 1
+        return 0 if firstTrueIdx == -1 else n - firstTrueIdx
+
         n = len(citations)
         count = [0]*(n+1)
         for idx, num in enumerate(citations):
