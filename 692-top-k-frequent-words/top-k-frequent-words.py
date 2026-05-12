4abad0d712dd47afee679c5a1a706c8ecfd0bc4c
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-05-12 23:59:20 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.30%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
         c = Counter(words)
         heap = [(-1*count, key) for key, count in c.items()]
         heapq.heapify(heap)
