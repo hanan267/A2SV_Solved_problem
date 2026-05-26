c7a41b3ecb67b77e83fccbf2b5681a42aa079d58
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-05-26 16:34:43 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.95%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
         def rob_linear(arr):
             prev1 = 0 
             prev2 = 0  
@@ -12,11 +11,8 @@ class Solution:
                 prev1 = curr
             return prev1
         n = len(nums)
-
         if n == 1:
             return nums[0]
-
         case1 = rob_linear(nums[:-1])  
         case2 = rob_linear(nums[1:])
-
         return max(case1, case2)
