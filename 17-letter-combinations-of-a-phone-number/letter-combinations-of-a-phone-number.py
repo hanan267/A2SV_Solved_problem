39bb955b67db3b62b5461ab944a90e6685b29af2
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-07-22 18:18:01 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.19%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,121 +2,29 @@ class Solution:
    
     def letterCombinations(self, digits: str) -> List[str]:
 
-        phone_map = {
+        if not digits:
+            return []
+
+        numDigit = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
-
-        if not digits:
-            return
-    
+        
         res = []
         n = len(digits)
-        def backTrack(start, temp):
-            if len(temp) == n:
-                res.append(temp)
-                return
+
+        def backTracking(idx, curr):
+            if len(curr) == n:
+                res.append(curr)
+                return 
+
+            letters = numDigit[digits[idx]]
+
+            for char in letters:
+                backTracking(idx+1, curr+char)
             
-            
-            for letter in phone_map[digits[start]]:
-                temp += letter
-                backTrack(start+1, temp)
-                temp = temp[:-1]
         
-        backTrack(0, "")
+        backTracking(0, "")        
         return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if not digits:
-        #     return []
-
-       
-
-        # phone_map = {
-        #     "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-        #     "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
-        # }
-
-        # result = []
-
         
-        # def backtrack(index, current):
-           
-        #     if index == len(digits):
-        #         result.append(current)
-        #         return
-
-            
-        #     for letter in phone_map[digits[index]]:
-        #         backtrack(index + 1, current + letter)
-
-       
-        # backtrack(0, "")
-        # return result
+        
