562e416601fdaf733f29a9c4f315f3eee2039b1f
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-07-23 19:15:03 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (72.15%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,17 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int):
-        result = []
-        def backtrack(path, open_count, close_count):
-            if len(path) == 2 * n:
-                result.append("".join(path))
+
+        res = []
+
+        def backTrack(curr, open, close):
+
+            if len(curr) == 2 * n:
+                res.append(curr)
                 return
-            if open_count < n:
-                path.append("(")
-                backtrack(path, open_count + 1, close_count)
-                path.pop()
-            if close_count < open_count:
-                path.append(")")
-                backtrack(path, open_count, close_count + 1)
-                path.pop()
-        backtrack([], 0, 0)
-        return result
+            
+            if open < n:
+                backTrack(curr + "(", open+1, close)
+            
+            if close < open:
+                backTrack(curr + ")", open, close+1)
+        
+        backTrack("", 0, 0)
+        return res
+       
