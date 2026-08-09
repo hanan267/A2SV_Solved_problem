9a1bcbb2bca66ec99fd70c65de9e8e6982f45317
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-09 22:22:26 +0300

    Time: 22 ms (79.91%) | Memory: 33.5 MB (74.18%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,10 +4,12 @@ class Solution:
 
         slow = 0
         fast = 0
+        fast = 0
 
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
+
 
             if slow == fast: # cycle exists
                 break
@@ -17,15 +19,12 @@ class Solution:
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
-
         return slow
 
         n = len(nums)
         realNum = [0]*(n-1)
-
         for i in range(n):
             correct = nums[i] - 1
-          
             if realNum[correct] == 0:
                 realNum[correct] = nums[i]
             else:
