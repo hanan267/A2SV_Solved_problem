393f92fa6a8bb57625d492e8ec6dc0c18f74e964
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-14 19:57:48 +0300

    Time: 85 ms (68.35%) | Memory: 22.4 MB (25.12%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -11,7 +11,6 @@ class Solution:
                 count += 1
             else:
                 count = 1
-            
             if count <= 2:
                 nums[j] = nums[i]
                 j += 1
