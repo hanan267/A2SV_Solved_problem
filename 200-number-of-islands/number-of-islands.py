e2cda08c67ce0a34c5bc22b7d2fe99c7be84dbd7
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-15 22:35:30 +0300

    Time: 248 ms (46.29%) | Memory: 21.8 MB (35.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,5 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
+
         def inbound(i, j):
             return (0 <= i < len(grid) and 0 <= j < len(grid[0]))
 
@@ -12,7 +14,6 @@ class Solution:
                 dfs(i, j-1)
                 dfs(i+1, j)
                 dfs(i-1, j)
-
         island = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
