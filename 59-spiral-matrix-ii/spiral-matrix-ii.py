5cb8915e18f3480a3d3c5fb804091c66aec1d374
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-15 15:28:05 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (10.64%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+
+    
+
+        a = [[0]*n for i in range(n)]
+        t = 0
+        b = n - 1
+        l = 0
+
+        r = n- 1
+        x = 1
+        while t <= b and l <= r:
+            for j in range(l, r + 1):
+                a[t][j] = x
+                x += 1
+            t += 1
+            for i in range(t, b + 1):
+                a[i][r] = x
+                x += 1
+            r -= 1
+            if t <= b:
+                for j in range(r, l - 1, -1):
+                    a[b][j] = x
+                    x += 1
+                b -= 1
+            if l <= r:
+                for i in range(b, t - 1, -1):
+                    a[i][l] = x
+                    x += 1
+                l += 1
+        return a
