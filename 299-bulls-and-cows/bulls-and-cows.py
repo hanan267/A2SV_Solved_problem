90615570e54734d77b491af711ba300823380397
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-16 14:13:06 +0300

    Time: 3 ms (87.03%) | Memory: 19.3 MB (71.19%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.py
+++ 299-bulls-and-cows/bulls-and-cows.py
@@ -2,16 +2,17 @@ class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         bulls = 0
         cows = 0
-        s = [0] * 10
-        g = [0] * 10
+        s = [0]*10
+        g = [0]*10
+
+
 
         for i in range(len(secret)):
-            if secret[i] == guess[i]:
-                bulls += 1
-            else:
-                s[int(secret[i])] += 1
-                g[int(guess[i])] += 1
-
+                if secret[i] == guess[i]:
+                    bulls += 1
+                else:
+                    s[int(secret[i])] += 1
+                    g[int(guess[i])] += 1
         for i in range(10):
             cows += min(s[i], g[i])
         return str(bulls) + "A" + str(cows) + "B"
