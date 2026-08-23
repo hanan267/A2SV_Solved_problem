870be755f307cc369cb7a858379dda81cd636c23
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-23 21:09:22 +0300

    Time: 57 ms (25.34%) | Memory: 19.5 MB (49.47%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,5 +1,7 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        
         if len(s1) + len(s2) != len(s3):
             return False
         dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
