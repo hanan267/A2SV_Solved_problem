1a7a9c87a07a146669f69e3e1346f4a238ff427b
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-26 20:38:39 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (64.67%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,12 +1,10 @@
-from functools import cmp_to_key
-from typing import List
-
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
     
         nums = list(map(str, nums))
 
         def compare(x, y):
+
             if x + y > y + x:
                 return -1
             elif x + y < y + x:
