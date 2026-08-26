26e73e71a303d7b40de62dbea270c33b15b02c48
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-08-26 20:38:14 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.30%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -3,28 +3,7 @@ from typing import List
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # # Convert all numbers to strings
-        # nums = list(map(str, nums))
-        
-        # # Comparator function
-        # def compare(x, y):
-        #     if x + y > y + x:
-        #         return -1  # x should come first
-        #     elif x + y < y + x:
-        #         return 1   # y should come first
-        #     else:
-        #         return 0   # equal
-        
-        # # Sort with custom comparator
-        # nums.sort(key=cmp_to_key(compare))
-        
-        # # Join the numbers into a single string
-        # result = "".join(nums)
-        
-        # # Handle the case where all numbers are zeros
-        # return "0" if result[0] == "0" else result
-
-
+    
         nums = list(map(str, nums))
 
         def compare(x, y):
@@ -36,7 +15,6 @@ class Solution:
                 return 0
         nums.sort(key=cmp_to_key(compare))
         
-
         return "0" if nums[0] == "0" else "".join(nums)
 
     
