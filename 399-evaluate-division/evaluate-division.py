4dab6bf5b1c2258b0f33ca987b2eeb80bc1010f5
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-09-06 19:53:07 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (40.36%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -2,14 +2,18 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
 
         graph = {}
+
         for i in range(len(equations)):
             a = equations[i][0]
             b = equations[i][1]
             value = values[i]
+
             if a not in graph:
                 graph[a] = []
+
             if b not in graph:
                 graph[b] = []
+
             graph[a].append((b, value))
             graph[b].append((a, 1 / value))
 
@@ -21,14 +25,18 @@ class Solution:
             if current == target:
                 return 1
             visited.add(current)
+
             for neighbor, value in graph[current]:
+                
                 if neighbor in visited:
                     continue
                 result = dfs(neighbor, target, visited)
                 if result != -1:
                     return value * result
             return -1
+
         answer = []
+
         for a, b in queries:
             answer.append(dfs(a, b, set()))
 
