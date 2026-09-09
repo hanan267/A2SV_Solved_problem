1b3083a9bc1226def76e532b2009c97c00a9e907
hanan267 <150916842+hanan267@users.noreply.github.com>
2026-09-09 15:11:54 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.05%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -8,22 +8,18 @@ class Solution:
         numDigit = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
-        }
+            }
         
         res = []
         n = len(digits)
-
         def backTracking(idx, curr):
             if len(curr) == n:
                 res.append(curr)
                 return 
 
             letters = numDigit[digits[idx]]
-
             for char in letters:
                 backTracking(idx+1, curr+char)
-            
-        
         backTracking(0, "")        
         return res
         
